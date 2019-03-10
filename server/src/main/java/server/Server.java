b5b04e042c30d0f0f865ff80aafb7d572afb2a01
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-10 20:51:51 +0000

    Basic Maven structure to start the project

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,13 @@
+package server;
+
+/**
+ * Web Server
+ *
+ */
+public class Server 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ server/src/test/java/server/ServerTest.java
@@ -0,0 +1,19 @@
+package server;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for server.
+ */
+public class ServerTest 
+{
+    /**
+     * Placeholder test
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
